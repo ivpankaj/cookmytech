abd9ea78559841d3da80e5e7613752dbc15be1e8
ivpankajoph <pankaj@onlinepromotionhouse.com>
2025-12-11 14:52:34 +0530

    asd

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export", // <-- Enable static export
+    trailingSlash: true,
 };
 
 export default nextConfig;
