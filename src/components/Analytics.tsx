5c7a32156b2561f856f43bdd29c26e5669c16930
ivpankaj <imvpankaj@gmail.com>
2026-08-31 21:07:45 +0530

    asd

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -3,7 +3,7 @@
 import Script from "next/script";
 
 export default function Analytics() {
-  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+  const gaId = process.env.NEXT_PUBLIC_GA_ID || "G-SDDRT80WRZ";
   const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
   const clarityId = process.env.NEXT_PUBLIC_CLARITY_ID;
 
